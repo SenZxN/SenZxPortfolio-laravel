25b28d86504252fd26b46e671c83939318022e94
SenZxN <senzxn.art@gmail.com>
2023-12-27 01:38:17 +0700

    master

--- public/js/homejas.js
+++ public/js/homejas.js
@@ -1,24 +1,52 @@
+let viewportWidth = 0
+
 window.addEventListener('scroll', function() {
+    const viewportWidth = window.innerWidth;
     var navbar = document.getElementById('jobsnamed');
     var navbar2 = document.getElementById('aboutme');
     var currentScrollY = document.documentElement.scrollTop;
-    
-    if (currentScrollY > 210) {
-        navbar.classList.remove('inactive');
-        navbar.classList.add('active');
-        if (currentScrollY > 310) {
-            navbar2.classList.remove('inactive');
-            navbar2.classList.add('active');
-        }
 
+    console.log("Limited window width:", viewportWidth);
+
+    if (viewportWidth < 630) {
+        if (currentScrollY > 190 - viewportWidth * 0.5) {
+            navbar.classList.remove('inactive');
+            navbar.classList.add('active');
+            if (currentScrollY > 320 - viewportWidth * 0.45) {
+                navbar2.classList.remove('inactive');
+                navbar2.classList.add('active');
+            }
+    
+            else {
+                navbar2.classList.remove('active');
+                navbar2.classList.add('inactive');
+            }
+        }
+    
         else {
-            navbar2.classList.remove('active');
-            navbar2.classList.add('inactive');
+            navbar.classList.remove('active');
+            navbar.classList.add('inactive');
         }
     }
 
     else {
-        navbar.classList.remove('active');
-        navbar.classList.add('inactive');
+        if (currentScrollY > 190) {
+            navbar.classList.remove('inactive');
+            navbar.classList.add('active');
+            if (currentScrollY > 320) {
+                navbar2.classList.remove('inactive');
+                navbar2.classList.add('active');
+            }
+    
+            else {
+                navbar2.classList.remove('active');
+                navbar2.classList.add('inactive');
+            }
+        }
+    
+        else {
+            navbar.classList.remove('active');
+            navbar.classList.add('inactive');
+        }
     }
 });
