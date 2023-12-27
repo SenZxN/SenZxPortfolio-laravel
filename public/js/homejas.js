d51491e82fadef96b3e84b5dcb27a4cbcd19bc50
SenZxN <senzxn.art@gmail.com>
2023-12-27 21:37:29 +0700

    master

--- public/js/homejas.js
+++ public/js/homejas.js
@@ -2,6 +2,7 @@ window.addEventListener('scroll', function() {
     const viewportWidth = window.innerWidth;
     var navbar = document.getElementById('jobsnamed');
     var navbar2 = document.getElementById('aboutme');
+    var content1pc1 = document.getElementById('profilepic');
     var currentScrollY = document.documentElement.scrollTop;
 
     console.log("Limited window width:", viewportWidth);
@@ -13,6 +14,15 @@ window.addEventListener('scroll', function() {
             if (currentScrollY > 320 - viewportWidth * 0.45) {
                 navbar2.classList.remove('inactive');
                 navbar2.classList.add('active');
+                if (currentScrollY > 550 - viewportWidth * 0.35) {
+                    content1pc1.classList.remove('inactive');
+                    content1pc1.classList.add('active');
+                }
+        
+                else {
+                    content1pc1.classList.remove('active');
+                    content1pc1.classList.add('inactive');
+                }
             }
     
             else {
@@ -28,18 +38,28 @@ window.addEventListener('scroll', function() {
     }
 
     else {
-        if (currentScrollY > 190) {
+        if (currentScrollY > 310) {
             navbar.classList.remove('inactive');
             navbar.classList.add('active');
-            if (currentScrollY > 320) {
+            if (currentScrollY > 400) {
                 navbar2.classList.remove('inactive');
                 navbar2.classList.add('active');
+                if (currentScrollY > 450) {
+                    content1pc1.classList.remove('inactive');
+                    content1pc1.classList.add('active');
+                }
+        
+                else {
+                    content1pc1.classList.remove('active');
+                    content1pc1.classList.add('inactive');
+                }
             }
     
             else {
                 navbar2.classList.remove('active');
                 navbar2.classList.add('inactive');
             }
+
         }
     
         else {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,15 @@
             <br>Based from Indonesia</p>
         </div>
         <div id="content">
-            <h1 id="jobsnamed" class="inactive">Profesional Graphics Artist</h1>
-            <p id="aboutme" class="inactive">Hi, my name is Sevian Dwi. I am a student at SMK N 1 Bantul, majoring in Visual Communication Design. I have 2 years of experience with 3D software, 5 years of experience with motion graphics, and 6 years of experience with video editing. I am also a member of the Ujwala Pictures team (school team work) as a VFX Artist and Motion Graphic Artist. I am interested in learning more about 3D modeling, 3D animation, and motion graphics. I plan to pursue a career as a 3D animator or motion graphics animator in the future.</p>
+            <div id="content1" class="content">
+                <div class="content">
+                    <h1 id="jobsnamed" class="inactive">Profesional Graphics Artist</h1>
+                    <p id="aboutme" class="inactive">Hi, my name is Sevian Dwi. I am a student at SMK N 1 Bantul, majoring in Visual Communication Design. I have 2 years of experience with 3D software, 5 years of experience with motion graphics, and 6 years of experience with video editing. I am also a member of the School team work as a VFX Artist and Motion Graphic Artist. I am interested in learning more about 3D modeling, 3D animation, and motion graphics. I plan to pursue a career as a 3D animator or motion graphics animator in the future.</p>
+                </div>
+                <div id="profilepic" class="inactive">
+                    <img id="profpc" src="/img/profilephoto.png" alt="">
+                </div>
+            </div>
         </div>
     </div>
 @endsection
